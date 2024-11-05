a0a4c8e847d45bd622787b7ce9de06de66e48010
Christina Sørensen <christina@cafkafk.com>
2024-11-05 10:21:17 +0100

    refactor: move confirmation dialogue into shared crate
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -1,3 +1,39 @@
 // SPDX-FileCopyrightText: 2024 Christina Sørensen
 //
 // SPDX-License-Identifier: EUPL-1.2
+
+const CONFIRMATION_YES: &str = "y";
+const CONFIRMATION_NO: &str = "n";
+
+/// Pauses execution, awaiting user confirmation
+pub async fn get_confirmation() -> bool {
+  use futures::StreamExt;
+  use tokio::io::stdin;
+  use tokio_util::codec::{FramedRead, LinesCodec};
+
+  let mut reader = FramedRead::new(stdin(), LinesCodec::new());
+
+  println!("Continue deploying (Y/n)");
+
+  let mut input_buffer = reader.next().await.transpose().unwrap().unwrap();
+
+  // Normalize input
+  input_buffer = input_buffer.to_lowercase();
+
+  // Remove newline/return from input
+  if let Some('\n') = input_buffer.chars().next_back() {
+    input_buffer.pop();
+  }
+  if let Some('\r') = input_buffer.chars().next_back() {
+    input_buffer.pop();
+  }
+
+  match input_buffer.as_str() {
+    CONFIRMATION_YES => true,
+    CONFIRMATION_NO => false,
+    _ => {
+      println!("Please answer before continuing");
+      Box::pin(get_confirmation()).await
+    }
+  }
+}
--- crates/metamorph/src/morph.rs
+++ crates/metamorph/src/morph.rs
@@ -72,42 +72,6 @@ where
     .collect()
 }
 
-const CONFIRMATION_YES: &str = "y";
-const CONFIRMATION_NO: &str = "n";
-
-/// Pauses execution, awaiting user confirmation
-async fn get_confirmation() -> bool {
-  use futures::StreamExt;
-  use tokio::io::stdin;
-  use tokio_util::codec::{FramedRead, LinesCodec};
-
-  let mut reader = FramedRead::new(stdin(), LinesCodec::new());
-
-  println!("Continue deploying (Y/n)");
-
-  let mut input_buffer = reader.next().await.transpose().unwrap().unwrap();
-
-  // Normalize input
-  input_buffer = input_buffer.to_lowercase();
-
-  // Remove newline/return from input
-  if let Some('\n') = input_buffer.chars().next_back() {
-    input_buffer.pop();
-  }
-  if let Some('\r') = input_buffer.chars().next_back() {
-    input_buffer.pop();
-  }
-
-  match input_buffer.as_str() {
-    CONFIRMATION_YES => true,
-    CONFIRMATION_NO => false,
-    _ => {
-      println!("Please answer before continuing");
-      Box::pin(get_confirmation()).await
-    }
-  }
-}
-
 pub async fn foreach_deploy_set<I, S>(config: Config, action: &'static str, args: I)
 where
   I: IntoIterator<Item = S> + std::marker::Send + Clone + 'static,
@@ -120,7 +84,7 @@ where
   for deploy_set in config.deploy_sets {
     // Check if deploy set wants confirmation to proceed
     if deploy_set.confirm {
-      match get_confirmation().await {
+      match common::get_confirmation().await {
         true => (),
         false => std::process::exit(0),
       }
